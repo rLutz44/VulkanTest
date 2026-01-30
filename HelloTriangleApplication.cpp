8b855593ca9561aeba149143f94d019afcc14307
rLutz44 <robertlutz35@yahoo.de>
2026-01-30 14:24:44 +0100

    Queue Families and finding the right device (the right GPU)

--- HelloTriangleApplication.cpp
+++ HelloTriangleApplication.cpp
@@ -27,7 +27,35 @@ void HelloTriangleApplication::initVulkan()
 }
 
 bool HelloTriangleApplication::isDeviceSuitable(VkPhysicalDevice device) {
-	return true;
+	QueueFamilyIndices indices = findQueueFamilies(device);
+
+	return indices.graphicsFamily.has_value();
+}
+
+QueueFamilyIndices HelloTriangleApplication::findQueueFamilies(VkPhysicalDevice device)
+{
+	QueueFamilyIndices indices;
+	
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+	int i = 0;
+	for (const auto& queueFamily : queueFamilies) {
+		
+		if (indices.isComplete()) {
+			break;
+		}
+		
+		if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			indices.graphicsFamily = i;
+		}
+		i++;
+	}
+
+	return indices;
 }
 
 void HelloTriangleApplication::pickPyhsicalDevice() {
--- HelloTriangleApplication.h
+++ HelloTriangleApplication.h
@@ -2,8 +2,18 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <optional>
 #include "global.h"
 #include <iostream>
+
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+
+	bool isComplete() {
+		return graphicsFamily.has_value();
+	}
+
+};
 
 class HelloTriangleApplication
 {
@@ -18,7 +28,8 @@ class HelloTriangleApplication
 		void createInstance();
 		void setupDebugMessenger();
 		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
-		bool isDeviceSuitable(VkPhysicalDevice device); // Fix: return type should be bool
+		bool isDeviceSuitable(VkPhysicalDevice device); 
+		QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 
 		void pickPyhsicalDevice();
 
