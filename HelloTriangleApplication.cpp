7e3bea2dcff4774eb824efd859d1c269339984fd
rLutz44 <robertlutz35@yahoo.de>
2026-02-01 15:18:09 +0100

    Creating the logical Device

--- HelloTriangleApplication.cpp
+++ HelloTriangleApplication.cpp
@@ -71,6 +71,32 @@ void HelloTriangleApplication::createLogicalDevice()
 	float queuePriority = 1.0f;
 	queueCreateInfo.pQueuePriorities = &queuePriority;
 
+	VkPhysicalDeviceFeatures deviceFeatures{};
+
+	VkDeviceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+	createInfo.pQueueCreateInfos = &queueCreateInfo;
+	createInfo.queueCreateInfoCount = 1;
+
+	createInfo.pEnabledFeatures = &deviceFeatures;
+
+	createInfo.enabledExtensionCount = 0;
+
+	if (enableValidationLayers) {
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	}
+	else {
+		createInfo.enabledLayerCount = 0;
+	}
+
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create logical device!");
+	}
+
+
+
 }
 
 void HelloTriangleApplication::pickPyhsicalDevice() {
@@ -116,6 +142,8 @@ void HelloTriangleApplication::cleanup()
 	glfwDestroyWindow(window);
 
 	glfwTerminate();
+
+	vkDestroyDevice(device, nullptr);
 }
 
 void HelloTriangleApplication::createInstance()
